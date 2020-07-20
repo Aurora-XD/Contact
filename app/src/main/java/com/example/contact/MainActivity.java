441b9631976ea54a71a8d99186616165077099c2
yangchen <chen.yang1@thoughtworks.com>
2020-07-20 17:34:49 +0800

    add Activity Practices button

--- app/src/main/java/com/example/contact/MainActivity.java
+++ app/src/main/java/com/example/contact/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.contact;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
