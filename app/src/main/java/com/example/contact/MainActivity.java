2bf9bed0e1e5ef97ff550a2b3da196821159cc90
yangchen <chen.yang1@thoughtworks.com>
2020-07-20 17:39:48 +0800

    add contact Activity using intent

--- app/src/main/java/com/example/contact/MainActivity.java
+++ app/src/main/java/com/example/contact/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.contact;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mbutton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        mbutton1 = findViewById(R.id.button_1);
+        mbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ContactActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
