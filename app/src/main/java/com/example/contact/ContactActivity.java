fc29a520288074ea9ed830a603fcf1670ffdd9e0
yangchen <chen.yang1@thoughtworks.com>
2020-07-20 21:33:57 +0800

    add contact activity

--- app/src/main/java/com/example/contact/ContactActivity.java
+++ app/src/main/java/com/example/contact/ContactActivity.java
@@ -1,14 +1,75 @@
 package com.example.contact;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ContactActivity extends AppCompatActivity {
+
+    static final int REQUEST_SELECT_CONTACT = 2;
+    private Button mButton2;
+    private TextView userInfo;
+
+    String username;
+    String number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        mButton2 = findViewById(R.id.button_2);
+        userInfo = findViewById(R.id.user_info);
+
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction("android.intent.action.PICK");
+                intent.addCategory("android.intent.category.DEFAULT");
+                intent.setType("vnd.android.cursor.dir/phone_v2");
+                startActivityForResult(intent,REQUEST_SELECT_CONTACT);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_SELECT_CONTACT){
+            if(data!=null){
+                Uri uri = data.getData();
+
+                ContentResolver contentResolver = getContentResolver();
+                Cursor cursor=null;
+                if(uri != null){
+                    cursor = contentResolver.query(uri,new String[]{"display_name","data1"},null,null,null);
+                }
+                while (cursor.moveToNext()){
+                    username = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                }
+                cursor.close();
+
+                userInfo.setText(username+" "+number);
+            }
+        }
     }
 }
